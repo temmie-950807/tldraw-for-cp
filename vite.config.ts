4f4c23c542a2bfbef00de9ea781480e22495f307
temmie <kapoo950807@gmail.com>
2024-09-05 16:44:46 +0800

    新增部署到 github page 需要的東西

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/tldraw-for-cp/',
+	build: {
+		chunkSizeWarningLimit: 10000
+	},
 	plugins: [react()],
 })
